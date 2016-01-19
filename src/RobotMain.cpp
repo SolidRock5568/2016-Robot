5cda4454bda662ff3abb5afcfcb4df72a21d4922
ksundberg-5568 <kristy.sundberg@outlook.com>
2016-01-19 17:03:34 -0600

    Begin implementation of control system

--- src/RobotMain.cpp
+++ src/RobotMain.cpp
@@ -2,15 +2,24 @@
 
 class Robot: public IterativeRobot
 {
-private:
+	Joystick controller;
+	Talon right_motor;
+	Talon left_motor;
+	Talon raise_motor;
+	Victor shoot_motor_1;
+	Victor shoot_motor_2;
 
 
-	void RobotInit()
+public:
+	Robot ():
+		controller(0),
+		right_motor(0),
+		left_motor(1),
+		raise_motor(2),
+		shoot_motor_1(3),
+		shoot_motor_2(4)
 	{
-
 	}
-
-
 
 	void AutonomousInit()
 	{
@@ -29,7 +38,11 @@ private:
 
 	void TeleopPeriodic()
 	{
+		float driveStraight = (controller.GetRawButton(1)) / 2;
+		float driveBackwards = -(controller.GetRawButton(2)) / 2;
 
+		right_motor.Set(driveStraight + driveBackwards);
+		left_motor.Set(driveStraight + driveBackwards);
 	}
 
 	void TestPeriodic()
@@ -38,4 +51,4 @@ private:
 	}
 };
 
-//START_ROBOT_CLASS(Robot)
+START_ROBOT_CLASS(Robot)
