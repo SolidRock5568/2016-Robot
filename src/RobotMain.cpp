ba357245c27438cc4ef97dd1c7653636a4c12f79
Nullsrc <nanosoldierenke@gmail.com>
2016-01-20 18:03:42 -0600

    Use #define'd values for buttons; add Debug mode

--- src/RobotMain.cpp
+++ src/RobotMain.cpp
@@ -1,5 +1,16 @@
 #include "WPILib.h"
 
+#define A_BUTTON 1
+#define B_BUTTON 2
+#define X_BUTTON 3
+#define Y_BUTTON 4
+#define L_BUMPER 5
+#define R_BUMPER 6
+#define SELECT_BUTTON 7
+#define START_BUTTON 8
+
+bool IS_DEBUGGING;
+
 class Robot: public IterativeRobot
 {
 	Joystick controller;
@@ -17,9 +28,7 @@ public:
 		left_motor(1),
 		raise_motor(2),
 		shoot_motor_1(3),
-		shoot_motor_2(4)
-	{
-	}
+		shoot_motor_2(4){}
 
 	void AutonomousInit()
 	{
@@ -35,14 +44,23 @@ public:
 	{
 
 	}
-
+	
 	void TeleopPeriodic()
 	{
-		float driveStraight = (controller.GetRawButton(1)) / 2;
-		float driveBackwards = -(controller.GetRawButton(2)) / 2;
+		
+		if(IS_DEBUGGING == true) {
+		
+			float driveStraight = (controller.GetRawButton(X_BUTTON)) / 2;
+			float driveBackwards = (controller.GetRawButton(B_BUTTON)) / 2;
+			float driveLeft = (controller.GetRawButton(Y_BUTTON)) / 2;
+			float driveRight = (controller.GetRawButton(A_BUTTON)) / 2;
 
-		right_motor.Set(driveStraight + driveBackwards);
-		left_motor.Set(driveStraight + driveBackwards);
+			right_motor.Set(driveStraight - driveBackwards + driveLeft - driveRight);
+			left_motor.Set(driveStraight - driveBackwards + driveLeft - driveRight);
+		}
+		if(controller.GetRawButton(SELECT_BUTTON)) {
+			IS_DEBUGGING = !IS_DEBUGGING;
+		}
 	}
 
 	void TestPeriodic()
