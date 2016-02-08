a534ff4a7d071fd9f79def35f060817b903075fc
ksundberg-5568 <kristy.sundberg@outlook.com>
2016-02-08 17:04:18 -0600

    Updated Code
    Updated general controls and wrote a sample Auto program

--- src/RobotMain.cpp
+++ src/RobotMain.cpp
@@ -9,16 +9,20 @@
 #define SELECT_BUTTON 7
 #define START_BUTTON 8
 
-bool IS_DEBUGGING;
-
 class Robot: public IterativeRobot
 {
+	IMAQdxSession session;
+	Image *frame;
+	IMAQdxError imaqError;
+
 	Joystick controller;
 	Talon right_motor;
 	Talon left_motor;
-	Talon raise_motor;
-	Victor shoot_motor_1;
-	Victor shoot_motor_2;
+	Jaguar raise_motor;
+	Talon shoot_motor_1;
+	Talon shoot_motor_2;
+	Victor kick_motor;
+	bool isDebugging;
 
 
 public:
@@ -28,7 +32,27 @@ public:
 		left_motor(1),
 		raise_motor(2),
 		shoot_motor_1(3),
-		shoot_motor_2(4){}
+		shoot_motor_2(4),
+		kick_motor(5),
+		isDebugging(false)
+		{
+		}
+
+	void RobotInit()override
+	{
+		 // create an image
+		frame = imaqCreateImage(IMAQ_IMAGE_RGB, 0);
+		//the camera name (ex "cam0") can be found through the roborio web interface
+		imaqError = IMAQdxOpenCamera("cam0", IMAQdxCameraControlModeController, &session);
+		if(imaqError != IMAQdxErrorSuccess) {
+			DriverStation::ReportError("IMAQdxOpenCamera error: " + std::to_string((long)imaqError) + "\n");
+		}
+		imaqError = IMAQdxConfigureGrab(session);
+		if(imaqError != IMAQdxErrorSuccess) {
+			DriverStation::ReportError("IMAQdxConfigureGrab error: " + std::to_string((long)imaqError) + "\n");
+		}
+
+	}
 
 	void AutonomousInit()
 	{
@@ -37,6 +61,16 @@ public:
 
 	void AutonomousPeriodic()
 	{
+		raise_motor.Set(-0.75);
+		Wait(1500);
+		raise_motor.Set(0);
+		Wait(500);
+		right_motor.Set(0.5);
+		left_motor.Set(0.5);
+		Wait(3000);
+		right_motor.Set(0);
+		left_motor.Set(0);
+
 
 	}
 
@@ -44,23 +78,100 @@ public:
 	{
 
 	}
+
 	
 	void TeleopPeriodic()
 	{
-		
-		if(IS_DEBUGGING == true) {
-		
-			float driveStraight = (controller.GetRawButton(X_BUTTON)) / 2;
-			float driveBackwards = (controller.GetRawButton(B_BUTTON)) / 2;
-			float driveLeft = (controller.GetRawButton(Y_BUTTON)) / 2;
-			float driveRight = (controller.GetRawButton(A_BUTTON)) / 2;
+		// acquire images
+		IMAQdxStartAcquisition(session);
+		// grab an image, draw the circle, and provide it for the camera server which will
+		// in turn send it to the dashboard.
+		while(IsOperatorControl() && IsEnabled()) {
+			IMAQdxGrab(session, frame, true, NULL);
+			if(imaqError != IMAQdxErrorSuccess)
+			{
+				DriverStation::ReportError("IMAQdxGrab error: " + std::to_string((long)imaqError) + "\n");
+			}
+			else
+			{
+			}
 
-			right_motor.Set(driveStraight - driveBackwards + driveLeft - driveRight);
-			left_motor.Set(driveStraight - driveBackwards + driveLeft - driveRight);
+			if(isDebugging == true)
+			{
+				float driveStraight = float(controller.GetRawButton(X_BUTTON));
+				float driveBackwards = float(controller.GetRawButton(B_BUTTON));
+				float driveLeft = float(controller.GetRawButton(Y_BUTTON));
+				float driveRight = float(controller.GetRawButton(A_BUTTON));
+
+				right_motor.Set(driveStraight - driveBackwards + driveLeft - driveRight);
+				left_motor.Set(driveStraight - driveBackwards + driveLeft - driveRight);
+			}
+			else if(controller.GetRawButton(SELECT_BUTTON) == true)
+			{
+				isDebugging = !isDebugging;
+			}
+
+			if(controller.GetRawAxis(3) > 0 )
+			{
+				shoot_motor_1.Set(1);
+				shoot_motor_2.Set(-1);
+			}
+			else if(controller.GetRawAxis(2) > 0 )
+			{
+				shoot_motor_1.Set(-0.4);
+				shoot_motor_2.Set(0.4);
+			}
+			else
+			{
+				shoot_motor_1.Set(0);
+				shoot_motor_2.Set(0);
+			}
+
+			if (controller.GetRawAxis(1) < -0.2)
+			{
+				float speed = controller.GetRawAxis(1) * controller.GetRawAxis(1);
+				raise_motor.Set(-speed);
+			}
+			else if (controller.GetRawAxis(1) > 0.2)
+			{
+				float speed = controller.GetRawAxis(1) * controller.GetRawAxis(1);
+				raise_motor.Set(speed);
+			}
+			else
+			{
+				raise_motor.Set(0);
+			}
+
+			if(controller.GetRawButton(B_BUTTON) == true)
+			{
+				kick_motor.Set(0.5);
+			}
+			else if(controller.GetRawButton(X_BUTTON) == true)
+			{
+				kick_motor.Set(-0.5);
+			}
+			else
+			{
+				kick_motor.Set(0);
+			}
+
+
+
+				float right_Stick_FB = controller.GetRawAxis(5);
+				float right_Stick_LR = controller.GetRawAxis(4);
+				right_Stick_FB = right_Stick_FB * right_Stick_FB * right_Stick_FB;
+				right_Stick_LR = right_Stick_LR * right_Stick_LR * right_Stick_LR;
+
+				right_motor.Set(right_Stick_FB + right_Stick_LR);
+				left_motor.Set(right_Stick_LR - right_Stick_FB);
+
+
+				Wait(0.005);				// wait for a motor update time
 		}
-		if(controller.GetRawButton(SELECT_BUTTON)) {
-			IS_DEBUGGING = !IS_DEBUGGING;
-		}
+		// stop image acquisition
+
+		IMAQdxStopAcquisition(session);
+
 	}
 
 	void TestPeriodic()
